d2b76dcfdc805fae8027020801553248ad96971b
Balogun Olatunji Johnson <olatunjibalogun38@yahoo.com>
2025-02-14 00:01:19 +0100

    feat:added rate limiter

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express= require("express");
 const mongoose= require("mongoose");
 const cors= require("cors");
 const authroutes= require("./routes/authrouthes");
+const limiter = require("express-rate-limit");
+const errorHandler = require("./Validation/middleware/errorhandler");
 // Initialize a express app
 const app = express();
 
@@ -12,7 +14,15 @@ app.use(cors());
 
 //ensure that express can read json
 app.use(express.json());
+const rateLimiter = limiter({
+    windowMs: 30 * 60 * 1000,
+    max: 50,
+    message: "please try again in 30mins"
+});
+app.use("/byway",rateLimiter);
 app.use("/byway",authroutes);
+app.use(errorHandler);
+
 //Mongo db connection
 mongoose.connect(process.env.MONGODB_URL)
 .then(() => {
